de17bba5ae9adfe9ba01cc4aa7fe5759f94e1d1c
Sufiyan-Khanzada <sufiyankhanzada1254@gmail.com>
2024-11-19 20:03:21 +0500

    create a cart model & create the cart migration & create the cart controller fucntions working with database added cart in the cart done count of cart done

---
+++ Modules/Cart/Entities/Carts.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Modules\Cart\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Base\Eloquent\Model;
+use Modules\Base\Traits\Sluggable;
+use Modules\Base\Eloquent\Translatable;
+use Modules\Author\Admin\Table\AuthorTable;
+use Modules\Files\Eloquent\HasMedia;
+use Modules\Files\Entities\Files;
+use Modules\User\Entities\User;
+use Modules\Ebook\Entities\Ebook;
+
+class Carts extends Model
+{
+    use HasMedia;
+
+    protected $fillable = ['user_id','ebook_id'];
+
+    
+  
+
+   
+}
